7ec1ac4e037441d8ce1ca7a9afb2b1a86c74dae1
oded <oded@dcs.bbk.ac.uk>
2012-10-24 23:44:51 +0100

    Test example

---
+++ Ex/src/FirstTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author Oded
+ *
+ */
+public class FirstTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
