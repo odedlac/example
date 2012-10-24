5e1bfe69a92738958275a7d9a65959cde10601e6
oded <oded@dcs.bbk.ac.uk>
2012-10-24 23:55:29 +0100

    New Mult class First Test has a real test that fails at this stage

--- Ex/src/FirstTest.java
+++ Ex/src/FirstTest.java
@@ -2,19 +2,38 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-/**
- * 
- */
+
 
 /**
  * @author Oded
  *
  */
+
+/** First test 
+ *  Mult class created
+ *  method operation still a stub
+ *  **/
+
+/**
+ * Test should fail
+ * 
+ */
+
 public class FirstTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		int input1 = 4;
+		int input2 = 7;
+		int expectedAnswer = 11;
+		int actualAnswer;
+		
+		
+		Mult classUnderTest = new Mult();
+		
+		actualAnswer = classUnderTest.operation(input1,input2);
+		
+		assertSame("Not the same",expectedAnswer, actualAnswer);
 	}
 
 }
---
+++ Ex/src/Mult.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+
+/**
+ * @author Oded
+ *
+ */
+public class Mult {
+
+	public int operation(int input1, int input2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
